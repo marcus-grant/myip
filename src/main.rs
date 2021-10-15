3fe08dc4c082a488e18f2c8fe873ec81cb96e7d3
Marcus Grant <marcusfg@gmail.com>
2021-10-16 01:47:24 +0200

    Ft: StructOpt parsing
    * server subcommand
      * runs the command as a server
      * next option is the port, defaults to 80
    * NONE subcommand
      * runs the command as a client
      * yet to be implemented
    * global flags:
      * -d --debug for debug mode
        * with printlns for all options
      * -h --help & help subcommand for help msg
    * printlns for server and client mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+extern crate structopt;
+// use std::net::TcpListener;
+// use std::env;
+use structopt::clap::crate_version;
+use structopt::StructOpt;
+
+
+// Good learning source for rust to build CLI programs below:
+// https://rust-cli.github.io/book/tutorial/cli-args.html
+// Used as template
+
+const USAGE_DESCRIPTION: &str =
+"A program that can operate both as a server to report the IP address of any HTTP GET request and a client to make that request.";
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "MyIP",
+    version = crate_version!(),
+    about = USAGE_DESCRIPTION,
+    author = "Marcus Grant",
+    )]
+struct Command {
+
+    #[structopt(subcommand)]
+    subcommand: Option<SubCommand>,
+
+    #[structopt(short, long, global = true)]
+    debug: bool,
+
+    /* TODO Implement proper logging
+    /// Activate logging
+    #[structopt(short, long, global = true)]
+    log: bool,
+    */
 }
+
+#[derive(Debug, StructOpt)]
+enum SubCommand {
+    /// Run as server
+    Server(ServerOpts),
+}
+
+#[derive(Debug, StructOpt)]
+struct ServerOpts {
+    /// Port to listen to
+    #[structopt(default_value = "80")]
+    port: u16,
+}
+
+fn main() {
+    // let listener = TcpListener::bind("127.0.kkk")
+    // let args: Vec<String> = env::args().collect();
+    let opts = Command::from_args();
+
+    if opts.debug {
+        println!("\n=== Debug Mode! ===\n");
+        println!("Options: {:?}\n", opts);
+    }
+
+    match opts.subcommand {
+        Some(SubCommand::Server(props)) => println!(
+            "Server started, listening on port {:?}", props.port),
+        None => println!("Client mode not yet implemented!"),
+    }
+}
+
